ed2d59a34257894d6d99a2c3d24ccd2f1d6a458b
Fredd89 <ficofedebla@gmail.com>
2022-02-10 11:48:18 +0100

    writeFile() method properly working.

--- app/src/main/java/itts/volta/scritturafile/MainActivity.java
+++ app/src/main/java/itts/volta/scritturafile/MainActivity.java
@@ -2,13 +2,43 @@ package itts.volta.scritturafile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtView;
+    Button readBtn, writeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtView = findViewById(R.id.txtView);
+        readBtn = findViewById(R.id.readBtn);
+        writeBtn = findViewById(R.id.writeBtn);
+        ManageFile manageFile = new ManageFile();
+        Context context = getApplicationContext();
+
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manageFile.readFile("Test.txt", getApplicationContext());
+            }
+        });
+
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = manageFile.writeFile("Test.txt", getApplicationContext());
+                Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/itts/volta/scritturafile/ManageFile.java
+++ app/src/main/java/itts/volta/scritturafile/ManageFile.java
@@ -1,33 +1,64 @@
 package itts.volta.scritturafile;
 
 import android.content.Context;
+import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class ManageFile {
+    public class ManageFile {
 
-    public String writeFile(String fileName, Context c) {
+        public String readFile(String fileName, Context c) {
 
-        String result = "";
-        FileOutputStream file;
-        String str = "Text to write into the file";
+            BufferedReader fileIn = null;
+            String outputFile;
+            StringBuilder strB = new StringBuilder();
 
-        try {
-            file = c.openFileOutput(fileName, Context.MODE_PRIVATE);
-            file.write(str.getBytes());
+            try{
+                fileIn= new BufferedReader(new InputStreamReader(c.openFileInput(fileName)));
+                while((outputFile = fileIn.readLine()) != null) {
+                    strB.append(outputFile + "\n");
+                }
+            }
+            catch(FileNotFoundException e){
+                Log.e("!ifFileExists", "Il file non esiste");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return strB.toString();
+
         }
 
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        public String writeFile(String fileName, Context c) {
 
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
+            String result = "";
+            FileOutputStream file;
+            String str = "Text to write into the file";
 
+            try
+            {
+                file = c.openFileOutput(fileName, Context.MODE_PRIVATE);
+                file.write(str.getBytes());
+                file.close();
+                result = "File letto correttamente";
+            }
+
+            catch (FileNotFoundException e) {
+                Log.e("ManageFile", "FNF error intercepted");
+                e.printStackTrace();
+                result = "File not found Exception";
+            }
+
+            catch (IOException e) {
+                Log.e("ManageFile", "IO error intercepted");
+                e.printStackTrace();
+                result = "Input Output Exception";
+            }
+            return result;
+        }
 }
